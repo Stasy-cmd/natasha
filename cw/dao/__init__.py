09f2c7246302a91d5d4fabcb179e197fe1e2ab75
milkyway03 <95718076+milkyway03@users.noreply.github.com>
2022-03-28 15:54:56 +0300

    Add files via upload

---
+++ cw/dao/__init__.py
@@ -0,0 +1,12 @@
+from .genre import GenreDAO
+from .director import DirectorDAO
+from .movie import MovieDAO
+from .user import UserDAO
+
+
+__all__ = [
+    "GenreDAO",
+    "DirectorDAO",
+    "MovieDAO",
+    "UserDAO"
+    ]
